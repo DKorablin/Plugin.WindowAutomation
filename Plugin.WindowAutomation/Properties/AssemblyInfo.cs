62de085017a1c3664d6bff2668a60477c8baf890
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 17:52:31 +0300

    Unify plugins deployment

--- Plugin.WindowAutomation/Properties/AssemblyInfo.cs
+++ Plugin.WindowAutomation/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("e8d66370-032d-453a-8bbd-7dbea49feb54")]
 [assembly: System.CLSCompliant(false)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/")]
 #else
 
-[assembly: AssemblyTitle("Plugin.WindowAutomation")]
 [assembly: AssemblyDescription("Browse and automate user events")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.WindowAutomation")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2021-2024")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2021-2025")]
 #endif
